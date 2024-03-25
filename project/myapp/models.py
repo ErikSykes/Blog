b4eb0af5fff65dde563b47e4fbe15ce789cabe7e
Erik Horváth <elementmen@gmail.com>
2024-03-25 23:57:39 +0100

    database_added

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+class Topic(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    type = models.ForeignKey('TopicType', on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+
+class FavoriteTopic(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+class TopicType(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+class Comment(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    body = models.CharField(max_length=255)
+    timestamp = models.DateTimeField()
+
