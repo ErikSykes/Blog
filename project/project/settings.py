74c73274ba57c2e3c0ae7bc705c44c4f81ba6016
valcso33 <164936023+valcso33@users.noreply.github.com>
2024-03-25 19:39:23 +0000

    yes

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3tt63u=w9$40=7zenc#jz*jr_h0(i-t#k(gykfvmtpjve3t^sx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['192.168.50.5']
 
 
 # Application definition
